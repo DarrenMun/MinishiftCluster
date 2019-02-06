cc92f5d61f46e395a3edbb031cf56cdb4e30b54f
DarrenMun <darren@kineticskunk.com>
2019-02-06 14:00:56 +0200

    update

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@application.route('/')
 def hello_world():
 	return '<h1>Flask GIIIITTTTT</h1>'
 
 if __name__ == '__main__':
-	app.run()
+	application.run()
