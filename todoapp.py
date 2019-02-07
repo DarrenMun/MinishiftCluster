ffc905250d3eaca766ceff6f25076e2690b68acd
DarrenMun <darren@kineticskunk.com>
2019-02-07 10:49:39 +0200

    Updated db

---
+++ todoapp.py
@@ -0,0 +1,97 @@
+from flask import Flask
+
+application = Flask(__name__)
+application.config.from_pyfile('todoapp.cfg')
+application.config['PROPAGATE_EXCEPTIOINS'] = True
+db = SQLAlchemy(application)
+
+class Student(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(50), nullable= False)
+	physics = db.Column(db.Integer)
+	maths = db.Column(db.Integer)
+	chemistry = db.Column(db.Integer)
+
+@application.route('/')
+def hello_world():
+	return '<h1>Updated File</h1>'
+
+
+@application.route('/', methods=["GET"])
+def retreive_results():
+	data = Student.query.all()
+
+	output = []
+
+	for x in data:
+		student_data = {}
+		student_data['id'] = x.id
+		student_data['name'] = x.name
+		student_data['physics'] = x.physics
+		student_data['maths'] = x.maths
+		student_data['chemistry'] = x.chemistry
+		output.append(student_data)
+
+	return jsonify({'student': output})
+
+@application.route('/results', methods=['POST'])
+def add_results():
+	data = request.get_json()
+
+	if not data or not 'name' in data:
+		abort(400)
+
+	new_student = Student(name = data['name'],physics= data['physics'],maths= data['maths'], chemistry=data['chemistry'])
+	db.session.add(new_student)
+	db.session.commit()
+
+	return jsonify({'students':'New Student Created'}), 201
+
+@application.route('/results/<int:indexId>',methods=["GET"])
+def get_one_student(indexId):
+
+	student = Student.query.filter_by(id = indexId).first()
+
+	if not student:
+		return jsonify({'message':'No user found'})
+
+	student_data = {}
+	student_data['id'] = student.id
+	student_data['name'] = student.name
+	student_data['physics'] = student.physics
+	student_data['maths'] = student.maths
+	student_data['chemistry'] = student.chemistry
+
+	return jsonify({'student':student_data})
+
+@application.route('/results/<int:indexId>', methods=['DELETE'])
+def delete_student(indexId):
+
+	student = Student.query.filter_by(id = indexId).first()
+
+	if not student:
+		return jsonify({'message':'No user found'})
+
+	db.session.delete(student)
+	db.session.commit()
+
+	return jsonify({'message':'Student found and Deleted'})
+
+@application.route('/results/<int:indexId>', methods=['PUT'])
+def update_results(indexId):
+	
+	student = Student.query.filter_by(id = indexId).first()
+
+	if not student:
+		return jsonify({'message' : 'No Student found'})
+
+	student.name = request.json['name']
+	student.physics = request.json.get('physics', "")
+	student.maths = request.json.get('maths', "")
+	student.chemistry = request.json.get('chemistry', "")	
+	db.session.commit()
+	
+	return jsonify({'student':'Pass'})
+
+if __name__ == '__main__':
+	application.run()
